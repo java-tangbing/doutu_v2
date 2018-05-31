3ce4ec07ce7d2e367d034409b67ad8b9a824c829
caozw <410935195@qq.com>
2018-05-31 14:15:29 +0800

    001.5

--- app/src/main/java/com/pufei/gxdt/module/news/activity/NewsFeedBackActivity.java
+++ app/src/main/java/com/pufei/gxdt/module/news/activity/NewsFeedBackActivity.java
@@ -9,8 +9,20 @@ import android.widget.TextView;
 import com.pufei.gxdt.R;
 import com.pufei.gxdt.base.BaseMvpActivity;
 import com.pufei.gxdt.base.BasePresenter;
+import com.pufei.gxdt.contents.Contents;
 import com.pufei.gxdt.module.news.adapter.NewsFeedBackAdapter;
 import com.pufei.gxdt.module.news.bean.NewsBean;
+import com.pufei.gxdt.module.news.bean.NoticeBean;
+import com.pufei.gxdt.module.news.presenter.NewsPresenter;
+import com.pufei.gxdt.module.news.view.NewsView;
+import com.pufei.gxdt.utils.KeyUtil;
+import com.pufei.gxdt.utils.NetWorkUtil;
+import com.pufei.gxdt.utils.RetrofitFactory;
+import com.pufei.gxdt.utils.SharedPreferencesUtil;
+import com.pufei.gxdt.utils.ToastUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +30,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class NewsFeedBackActivity extends BaseMvpActivity {
+public class NewsFeedBackActivity extends BaseMvpActivity<NewsPresenter> implements NewsView {
     @BindView(R.id.news_feedback_rv)
     RecyclerView recyclerView;
     @BindView(R.id.ll_title_left)
@@ -28,11 +40,14 @@ public class NewsFeedBackActivity extends BaseMvpActivity {
 
     NewsFeedBackAdapter newsFeedBackAdapter;
     private List<NewsBean.ResultBean> mlist;
+    private String auth = "";
 
     @Override
     public void initView() {
         textViewTitle.setText(getResources().getString(R.string.news_feedback));
         backlinearLayout.setVisibility(View.VISIBLE);
+        auth = SharedPreferencesUtil.getInstance().getString(Contents.STRING_AUTH);
+
         LinearLayoutManager layoutManage = new LinearLayoutManager(this);
         layoutManage.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManage);
@@ -41,15 +56,24 @@ public class NewsFeedBackActivity extends BaseMvpActivity {
     @Override
     public void getData() {
         mlist = new ArrayList<>();
-//        for (int i = 0; i < 5; i++) {
-//            NewsBean bean = new NewsBean();
-//            mlist.add(bean);
-//        }
         newsFeedBackAdapter = new NewsFeedBackAdapter(mlist);
-//        newsSystemAdapter.setOnItemClickListener(this);
-//        discoverAdapter.addHeaderView(videoHeaderView);
         recyclerView.setAdapter(newsFeedBackAdapter);
-//        setAdapter();
+        setMyAdapter();
+    }
+
+    public void setMyAdapter() {
+        JSONObject jsonObject = KeyUtil.getJson(this);
+        try {
+            jsonObject.put("auth", auth);
+            jsonObject.put("type", 2);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        if (NetWorkUtil.isNetworkConnected(this)) {
+            presenter.newsNoticeContent(RetrofitFactory.getRequestBody(jsonObject.toString()));
+        } else {
+            ToastUtils.showShort(this, "请检查网络设置");
+        }
     }
 
     @Override
@@ -57,11 +81,6 @@ public class NewsFeedBackActivity extends BaseMvpActivity {
         return R.layout.activity_news_feedback;
     }
 
-    @Override
-    public void setPresenter(BasePresenter presenter) {
-//        this.presenter = presenter;
-
-    }
 
     @OnClick(R.id.ll_title_left)
     public void onViewClicked(View v) {
@@ -69,6 +88,27 @@ public class NewsFeedBackActivity extends BaseMvpActivity {
             case R.id.ll_title_left:
                 finish();
                 break;
+        }
+    }
+
+    @Override
+    public void setPresenter(NewsPresenter presenter) {
+        if (presenter == null) {
+            this.presenter = new NewsPresenter();
+            this.presenter.attachView(this);
+        }
+    }
+
+    @Override
+    public void getNoticeList(NoticeBean bean) {
+
+    }
+
+    @Override
+    public void getsNoticeContent(NewsBean bean) {
+        if (bean.getResult().size() > 0) {
+            mlist.addAll(bean.getResult());
+            newsFeedBackAdapter.notifyDataSetChanged();
         }
     }
 }
