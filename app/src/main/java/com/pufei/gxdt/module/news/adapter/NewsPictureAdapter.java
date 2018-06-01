1902ab2e75c4507c28ed0f6273e2d7accb6fb080
chenhx <chenhouxin@perfay.com>
2018-06-01 15:33:44 +0800

    modify conver

--- app/src/main/java/com/pufei/gxdt/module/news/adapter/NewsPictureAdapter.java
+++ app/src/main/java/com/pufei/gxdt/module/news/adapter/NewsPictureAdapter.java
@@ -24,8 +24,7 @@ public class NewsPictureAdapter extends BaseQuickAdapter<NewsBean.ResultBean, Ba
     @Override
     protected void convert(BaseViewHolder helper, NewsBean.ResultBean item) {
         helper.setText(R.id.news_picture_item_content, item.getContent())
-//
-        helper.setText(R.id.news_picture_item_content, item.getContent());
+                .setText(R.id.tv_dateLine,item.getDateline());
         GlideApp.with(mContext).load(item.getUrl())
 //                .placeholder(R.mipmap.my_uer_picture)
                 .into((ImageView) helper.getView(R.id.news_picture_item_url));
